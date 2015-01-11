bdcec142aea841ea22b4f48647ecf8eb94925cc8
Ricardo Murillo <rcjmurillo@gmail.com>
2015-01-11 17:51:53 -0600

    More interesting default pattern

--- main.go
+++ main.go
@@ -11,8 +11,14 @@ func main() {
 	signals := make(chan os.Signal, 1)
 	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
 
-	g := game.NewGame(15, 15)
-	g.Seed([]game.Point{{6, 7}, {7, 5}, {7, 6}, {7, 8}, {7, 9}, {8, 7}})
+	g := game.NewGame(41, 41)
+	g.Seed([]game.Point{
+		{17, 21}, {18, 20}, {18, 22}, {19, 21},
+		{19, 19}, {19, 23},
+		{20, 19}, {20, 23},
+		{21, 19}, {21, 23},
+		{23, 21}, {22, 20}, {22, 22}, {21, 21},
+	})
 	g.Start()
 
 	done := make(chan bool)
