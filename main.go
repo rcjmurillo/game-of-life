45e005cccb48465685d5f12d4f11780bb50688d8
Ricardo Murillo <rcjmurillo@gmail.com>
2015-01-11 15:23:21 -0600

    Stop with signals

--- main.go
+++ main.go
@@ -2,13 +2,24 @@ package main
 
 import (
 	"github.com/rcjmurillo/game-of-life/game"
-	"time"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+
 	g := game.NewGame(15, 15)
 	g.Seed([]game.Point{{6, 7}, {7, 5}, {7, 6}, {7, 8}, {7, 9}, {8, 7}})
 	g.Start()
-	time.Sleep(40 * time.Second)
-	g.Stop()
+
+	done := make(chan bool)
+	go func() {
+		<-signals
+		g.Stop()
+		done <- true
+	}()
+	<-done
 }
